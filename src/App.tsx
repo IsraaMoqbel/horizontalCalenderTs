4773c5b0a5684fcc415a2055fa01f49e469fb67a
Israa Mo <isramm94@gmail.com>
2022-07-23 14:04:22 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import HorizontalCalender from "./components/horizontalCalendar";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <HorizontalCalender />
+    </div>
+  );
+}
